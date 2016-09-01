f9f8ec2c9a066c35e72c568ecb0cc5bbd777a799
Kieran Gillibrand <Kieran.Gillibrand6@gmail.com>
2016-09-01 15:47:24 -0400

    Implementation mostly done
    Completley untested though.
    
    Parsing different kernel version strings (from different distros) will probably be messy.
    
    Still need to implement main method.

--- Nvidia Signing Script.py
+++ Nvidia Signing Script.py
@@ -50,10 +50,12 @@ __status__ = 'Personal Project (in development)'
 #Imports
 import sys
 
+import subprocess
+
 #Code
 def handleError (errorMessage: str, exitCode: int)
 '''
-    Error handling function which displays a message and exits with an exit code.
+    Error handling function which displays a message and exits with an exit code (void).
     
     message (str): The message to display before exiting
     exitCode (int): The code to exit with
@@ -66,15 +68,16 @@ def handleError (errorMessage: str, exitCode: int)
 
 def getPackageManager () -> str:
     '''
-        Returns the package manager for the current system or exits if it cannot be found.
+        Returns the package manager for the current system or exits if it cannot be found (str).
         There is no standard way to find the package manager so this function simply tests if the following are installed: dpkg, dnf, yum, pacman
     '''
     
     #Each branch tries to run the package manager by checking it's version.
     #The package manager itself (ex: rpm) is called instead of a front end (ex: dnf) so more systems are covered
-    #An argument like that should be one of the fastest for a package manager and returns an exit status of 0.
+    #Each is called with the --version argument since it should be one of the fastest for a package manager and returns an exit status of 0.
     #Just running the package manager with no arguments usually returns 1 in my experience even if it is installed.
-    #If the package manager is not installed a status of 127 is usually returned.
+    #If the package manager is not installed a status of 127 is usually returned, either way we just look for 0.
+    #As is this will cause issues if multiple package managers are installed because only the first that is checked will be detected.
     
     #dpkg (Debian based systems)
     if os.system ('dpkg --version') == 0:
@@ -82,7 +85,7 @@ def getPackageManager () -> str:
         
     #rpm (Red hat based systems)
     else if os.system ('rpm --version') == 0:
-        return 'dnf'
+        return 'rpm'
         
     #pacman (Arch linux based systems)
     else if os.system ('pacman --version') == 0:
@@ -94,37 +97,125 @@ def getPackageManager () -> str:
     
 def getInstalledKernels (packageManager: str) -> list:
     '''
-        Returns a list of the currently installed kernels
+        Returns a list of the currently installed kernels (list <str>).
+        Calls a command depending on the package manager to list all installed kernels.
+        
+        packageManager (str): The system package manager fetched by getPackageManager ()
     '''
     
+    outputString = None
+    
+    if packageManager == 'dpkg':
+        output = subprocess.check_output (['dpkg', '--list', '|', 'grep', 'linux-image'])
+        outputString = output.decode ('utf-8')
+        
+    else if packageManager == 'rpm':
+        output = subprocess.check_output (['rpm', '-qa', 'kernel'])
+        outputString = output.decode ('utf-8')
+        
+    else if packageManager == 'pacman':
+        output = subprocess.check_output (['pacman', '-Q', '|', 'grep', 'linux'])
+        outputString = output.decode ('utf-8')
+        
+    print (outputString)
+    
+    #Need to parse output
+    
+def compareKernels (kernel1: str, kernel2: str) -> int:
+    '''
+        Compares two kernel version strings (ex: 4.7.2-200 vs 4.5.5).
+        Returns 1 if the first is newer (greater), -1 if the second is newer, and 0 if they are equal (int).
+        Compares the first version strings (ex: 4.7.2) and the second (ex: -200) if the first two are equal.
+        
+        kernel1 (str): The first kernel
+        kernel2 (str): The second kernel
+    '''
+    
+    firstVersion1 = kernel1 [0:4]
+    secondVersion1 = kernel1 [6:8]
+    
+    firstVersion2 = kernel2 [0:4]
+    secondVersion2 = kernel2 [6:8]
+    
+    print (firstVersion1)
+    print (secondVersion1)
+    
+    print (firstVersion2)
+    print (secondVersion2)
+    
+    firstVersionNumber1 = int (firstVersion1)
+    secondVersionNumber1 = int (secondVersion1)
+    
+    firstVersionNumber2 = int (firstVersion2)
+    secondVersionNumber2 = int (secondVersion2)
+    
+    if firstVersionNumber1 > firstVersionNumber2:
+        return 1
+    
+    else if firstVersionNumber1 < firstVersionNumber2:
+        return -1
+        
+    else:
+        if secondVersionNumber1 > secondVersionNumber2:
+            return 1
+            
+        else if secondVersionNumber1 < secondVersionNumber2:
+            return -1
+            
+        else:
+            return 0
+             
 def getNewKernels (currentKernel: str, installedKernels: list) -> list:
     '''
-        Returns a list of all kernels that are newer than the currently booted one or exits if no kernels are newer than the currently booted one.
+        Returns a list of all kernels that are newer than the currently booted one or exits if no kernels are newer than the currently booted one (list <str>)
         
         currentKernel (str): The currently booted kernel
-        installedKernels (list): A list of all the currently installed kernels
+        installedKernels (list <str>): A list of all the currently installed kernels
     '''
     
-    SIGN_PATH = '/usr/src/kernels/' + currentKernel + '/scripts/sign-file'
-    '''Path to the sign-file binary '''
+    currentKernel= subprocess.check_output (['uname', '-r'])
     
-def signKernel (kernel: str):
+    for installedKernel in installedKernels:
+        returnValue = compareKernels (installedKernel, currentKernel)
+        
+        if returnValue == -1 || returnValue == 0:
+            installedKernels.remove (installedKernel)
+    
+    return installedKernels
+    
+def signKernel (kernel: str, privateKeyPath: str, publicKeyPath: str):
     '''
-        Signs the nvidia kernel modules for a kernel
+        Signs the nvidia kernel modules for a kernel (void).
         
         kernel (str): The kernel whose modules to sign
+        privateKeyPath (str): The path to the private key file to sign with
+        publicKeyPath (str): The path to the public key file to sign with
     '''
+    
+    SIGN_BINARY_PATH = '/usr/src/kernels/' + kernel + '/scripts/sign-file'
+    MODULES_PATH = '/usr/lib/modules/' + kernel + '/extra/nvidia/'
+    MODULES = ['nvidia-drm.ko', 'nvidia.ko', 'nvidia-modeset.ko', 'nvidia-uvm.ko']
+    
+    subprocess.check_output ([SIGN_BINARY_PATH, 'sha256', privateKeyPath, publicKeyPath, MODULES_PATH + MODULES [0]])
+    subprocess.check_output ([SIGN_BINARY_PATH, 'sha256', privateKeyPath, publicKeyPath, MODULES_PATH + MODULES [1]])
+    subprocess.check_output ([SIGN_BINARY_PATH, 'sha256', privateKeyPath, publicKeyPath, MODULES_PATH + MODULES [2]])
+    subprocess.check_output ([SIGN_BINARY_PATH, 'sha256', privateKeyPath, publicKeyPath, MODULES_PATH + MODULES [3]])
 
-def signNewKernels (newKernels: list):
+def signNewKernels (newKernels: list, privateKeyPath: str, publicKeyPath: str):
     '''
-        Signs the nvidia kernel modules for every new kernel by calling signKernel () on each
+        Signs the nvidia kernel modules for every new kernel by calling signKernel () on each (void).
         
-        newKernels (list): A list of the new kernels to sign
+        newKernels (list <str>): A list of the new kernels to sign
+        privateKeyPath (str): The path to the private key file to sign with
+        publicKeyPath (str): The path to the public key file to sign with
     '''
-
+    
+    for newKernel in newKernels:
+        signKernel (newKernel, privateKeyPath, publicKeyPath)
+    
 def main ():
     '''
-        Main method for this script
+        Main method for this script (void).
     '''
     
 if __name__ == '__main__'
